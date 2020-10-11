9a0f3aa86b86e76c281aa64354e8f9575a17b58d
An-prog-hub <ac.ankushchauhan.2006@gmail.com>
2020-10-11 18:28:13 +0530

    add addnumber.c file

---
+++ Languages/C/addnumber.c
@@ -0,0 +1,20 @@
+// Program to Add Two Integers
+
+#include <stdio.h>
+int main() {    
+
+    int number1, number2, sum;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    // calculating sum
+    sum = number1 + number2;      
+    
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0;
+}
+
+// Enter two integers: 12
+// 11
+// 12 + 11 = 23
